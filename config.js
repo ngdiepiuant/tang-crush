7a5d8db43714a3f7a537fec516a0dafce4df908e
ngdiepiuant <91109063+ngdiepiuant@users.noreply.github.com>
2021-09-21 11:30:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const CONFIG = {
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
     title: 'Cảm ơn Tứn vì thời gian qua vẫn luôn bên cạnh iem nghe iem lải nhải',
-    desc: 'Cảm ơn Tứn vì đã kéo 1 con bot như iem lên quán quân ;))',
+    desc: 'Cảm ơn Tứn vì đã kéo 1 con bot như iem lên quán quân ;))
     btnYes: 'Tứn cũng vui khi bên iem',
     btnNo: 'hok Tứn hok thích em',
     question:'Sau này Tứn dẫn iem đi ăn bún đậu nhe =))',
