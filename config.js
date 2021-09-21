4023cfc3928335a5a52e28c9b504dfcdd4062b7e
ngdiepiuant <91109063+ngdiepiuant@users.noreply.github.com>
2021-09-21 11:34:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Tứn ơi 😳',
+    introTitle: 'Bab à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao anh cứ đi nhầm đường
-    Lạc vào tim iem lẻ loi
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Cảm ơn Tứn vì thời gian qua vẫn luôn bên cạnh iem nghe iem lải nhải',
-    desc: 'Cảm ơn Tứn vì đã kéo 1 con bot như iem lên quán quân ;))
-    btnYes: 'Tứn cũng vui khi bên iem',
-    btnNo: 'hok Tứn hok thích em',
-    question:'Sau này Tứn dẫn iem đi ăn bún đậu nhe =))',
-    btnReply: 'Gửi cho SinhToBo',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích anh <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'hehe',
-    messDesc: 'ily.',
-   
-    messLink: 'https://www.facebook.com/ng.dgdiep/
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
